ec853d233d1b887872a249de3aa0629e98510ccf
MinhNhat1306 <lai.nhat.minh1306@gmail.com>
2025-03-09 10:08:28 +0700

    xoa header

--- lai_nhat_minh/lai_nhat_minh.cpp
+++ lai_nhat_minh/lai_nhat_minh.cpp
@@ -1,13 +1,13 @@
 ﻿#include <stdio.h>
 #include <iostream>
 #include "toan.h"
-
+int Cong(int a, int b);  
 int main()
 {
     int a, b;
     scanf_s("%d", &a);
     scanf_s("%d", &b);
     int x = Cong(a, b);
-    return 0;  // Nên trả về 0
+    return 0;  
 }
 
