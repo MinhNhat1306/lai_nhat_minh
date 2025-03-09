bf9f36ea33931b1d30f35d974d31cee58b881cd7
MinhNhat1306 <lai.nhat.minh1306@gmail.com>
2025-03-09 11:38:07 +0700

    array

--- lai_nhat_minh/lai_nhat_minh.cpp
+++ lai_nhat_minh/lai_nhat_minh.cpp
@@ -1,15 +1,16 @@
-﻿#include <stdio.h>
-#include <iostream>
-#include "toan.h"
+﻿#include <iostream>
+#include <stdio.h>
 
-int main()
-{
-    int x_1 = 1;
-    int y_2 = 2;
-    int x = Cong(x_1, y_2);
-    int y = Tru(x_1, y_2);
-    int z = Nhan(x_1, y_2);
-    float w = Chia(x_1, y_2);
-    return 0;  
+int main() {
+    int arr[] = { 1, 2, 99, 67, 20 };
+    int giatri_gc = arr[0];
+
+    for (int i = 1; i < 5; i++) {
+        if (giatri_gc < arr[i]) {
+            giatri_gc = arr[i];
+        }
+    }
+
+    printf("gia tri lon nhat trong mang: %d", giatri_gc);
+    return 0;
 }
-
