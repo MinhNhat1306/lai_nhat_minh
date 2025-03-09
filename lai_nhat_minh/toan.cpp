39305cff76a76812690583e92e6e4d4485b9807f
MinhNhat1306 <lai.nhat.minh1306@gmail.com>
2025-03-09 10:20:44 +0700

    thay ketqua

--- lai_nhat_minh/toan.cpp
+++ lai_nhat_minh/toan.cpp
@@ -3,28 +3,28 @@
 
 int Cong(int a, int b)
 {
-    int ketqua1 = a + b;
-    printf("Ket qua bang %d\n", ketqua1);
-    return ketqua1;
+    int ketqua = a + b;
+    printf("Ket qua bang %d\n", ketqua);
+    return ketqua;
 }
 
 int Tru(int a, int b)
 {
-    int ketqua2 = a - b;
-    printf("Ket qua bang %d\n", ketqua2);
-    return ketqua2;
+    int ketqua = a - b;
+    printf("Ket qua bang %d\n", ketqua);
+    return ketqua;
 }
 
 int Nhan(int a, int b)
 {
-    int ketqua3 = a * b;
-    printf("Ket qua bang %d\n", ketqua3);
-    return ketqua3;
+    int ketqua = a * b;
+    printf("Ket qua bang %d\n", ketqua);
+    return ketqua;
 }
 
 int Chia(int a, int b)
 {
-    int ketqua4 = a / b;
-    printf("Ket qua bang %d\n", ketqua4);
-    return ketqua4;
+    int ketqua = a / b;
+    printf("Ket qua bang %d\n", ketqua);
+    return ketqua;
 }
