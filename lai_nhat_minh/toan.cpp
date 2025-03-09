6a5c748134ae37a05cee822d872812b49c2ee39f
MinhNhat1306 <lai.nhat.minh1306@gmail.com>
2025-03-09 10:02:54 +0700

    sua code, them ham va header

---
+++ lai_nhat_minh/toan.cpp
@@ -0,0 +1,9 @@
+#include "toan.h"
+#include <stdio.h>
+
+int Cong(int a, int b)
+{
+    int ketqua = a + b;
+    printf("Ket qua bang %d\n", ketqua);
+    return ketqua;
+}
---
+++ lai_nhat_minh/toan.h
@@ -0,0 +1,7 @@
+﻿#ifndef TOAN_H
+#define TOAN_H
+
+int Cong(int a, int b);  // Chỉ khai báo
+
+#endif
+
