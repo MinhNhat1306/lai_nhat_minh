dceb968969c0def0f7264fe68e2c79c3e0f2e400
MinhNhat1306 <lai.nhat.minh1306@gmail.com>
2025-03-09 10:17:53 +0700

    bo sung them tru, nhan, chia

--- lai_nhat_minh/lai_nhat_minh.cpp
+++ lai_nhat_minh/lai_nhat_minh.cpp
@@ -2,12 +2,18 @@
 #include <iostream>
 #include "toan.h"
 int Cong(int a, int b);  
+int Tru(int a, int b);
+int Nhan(int a, int b);
+int Chia(int a, int b);
 int main()
 {
     int a, b;
     scanf_s("%d", &a);
     scanf_s("%d", &b);
     int x = Cong(a, b);
+    int y = Tru(a, b);
+    int z = Nhan(a, b);
+    int w = Chia(a, b);
     return 0;  
 }
 
--- lai_nhat_minh/toan.cpp
+++ lai_nhat_minh/toan.cpp
@@ -3,7 +3,28 @@
 
 int Cong(int a, int b)
 {
-    int ketqua = a + b;
-    printf("Ket qua bang %d\n", ketqua);
-    return ketqua;
+    int ketqua1 = a + b;
+    printf("Ket qua bang %d\n", ketqua1);
+    return ketqua1;
+}
+
+int Tru(int a, int b)
+{
+    int ketqua2 = a - b;
+    printf("Ket qua bang %d\n", ketqua2);
+    return ketqua2;
+}
+
+int Nhan(int a, int b)
+{
+    int ketqua3 = a * b;
+    printf("Ket qua bang %d\n", ketqua3);
+    return ketqua3;
+}
+
+int Chia(int a, int b)
+{
+    int ketqua4 = a / b;
+    printf("Ket qua bang %d\n", ketqua4);
+    return ketqua4;
 }
