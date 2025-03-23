ab821d1703db761d724849b08c8157be43a96399
MinhNhat1306 <lai.nhat.minh1306@gmail.com>
2025-03-23 09:51:37 +0700

    luu lai

--- lai_nhat_minh/lai_nhat_minh.cpp
+++ lai_nhat_minh/lai_nhat_minh.cpp
@@ -1,6 +1,17 @@
 ﻿#include <stdio.h>
 
 void main() {
+    char x = 0x12345678;
+    char* px;
+    // chức năng 1: lưu đia chỉ của biến x
+    px = &x;
+    // chức năng 2: truy cập tới vùng nhớ có địa chỉ được lưu trong con trỏ
+    * px = 1;
+
+    printf("x: %d\n", x);
+}
+
+/*void main() {
     char str[] = "hello";
     int i = 0;
 
@@ -11,5 +22,4 @@ void main() {
         i++;
     }
     printf("Chuoi sau khi chuyen doi: %s", str);
-}
-
+}*/
